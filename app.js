2af737c04a667706e6268d77ee39336668e97c19
Brian Liu <86639831+brianliu8709@users.noreply.github.com>
2023-11-11 07:05:23 -0500

    initialized database connection (not fully implemented yet)

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// const mongoose = require('mongoose')
-// const personSchema = new mongoose.Schema({email: String}, { strict: false })
-// const Person = mongoose.model('grades', personSchema);
-// mongoose.connection.on('connected', () => {
-//     console.log('Mongoose is connected!')
-// })
+const mongoose = require('mongoose')
+const uri = "mongodb+srv://brianliu8709:hack-umass-lendit!@cluster0.nkvb3ht.mongodb.net/?retryWrites=true&w=majority"
+
+// defines the ItemRequets object in MongoDB, params to be specified
+const itemRequestSchema = new mongoose.Schema({
+    itemName: String
+}, { strict: false })
+const ItemRequest = mongoose.model('ItemRequest', itemRequestSchema);
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!')
+})
+
+// inserts an ItemRequest entry into the MongoDB, params to be specified
+const addItemRequest = async (itemName) => {
+    try {
+        await mongoose.connect(uri)
+    } catch (err) {
+        console.log('error: ' + err)
+    }
+    await ItemRequest.create({itemName: itemName})
+}
 
 app.use(express.json());
 
@@ -18,17 +33,12 @@ app.use(express.static( path.join(__dirname, 'public')))
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '/views'))
 
-// const loadHTMLLab = lab => {
-//     app.get(lab.url, (req, res) => {
-//       res.render('lessonTemplate.ejs', {current: lab, lessons: lessons})
-//     })
-// }
 
 // app.get('/index', (req, res) => {
 //     res.render('index')
 // })
 
-app.get('*', (req, res) => {
+app.get('*', async (req, res) => {
     res.send('hello world')
 })
 
