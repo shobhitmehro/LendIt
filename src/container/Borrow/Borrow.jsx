b15d7cfb69bd6ddbf6cb2f23be98ab7618d4cb7d
Shobhit Mehrotra <shobhit.m@icloud.com>
2023-11-11 21:46:35 -0500

    connect Borrow frontend with backend

--- src/container/Borrow/Borrow.jsx
+++ src/container/Borrow/Borrow.jsx
@@ -22,59 +22,51 @@ const Borrow = () => {
       // console.log(text)
       const body = await response.json()
       console.log(body)
-      setBorrowList(body)
+      setBorrowList(JSON.parse(body))
+
     }
   useEffect(() => {
     fetchData()
   })
 
+  const handleButtonClick = () => {
+
+  }
+
   return (
-    <div id='borrow' className='app__borrow '>
-        <div className='app__borrow-title-btn'>
-          <h4 className='head-text'>Borrow Requests</h4>
-        </div>
+    <div>
+   
+      <div id='borrow' className='app__borrow '>
+      <div className='app__borrow-title-btn'>
+        <h4 className='head-text'>Borrow Requests</h4>
+      </div>
 
-        <div classname='app__borrow-search'>
-          <input className='search-bar'
-            type="text"
-            placeholder="Search here"
-            //backend onChange={handleChange}
-            //backend value={searchInput} 
-            />
-        </div>
+      <div classname='app__borrow-search'>
+        <input className='search-bar'
+          type="text"
+          placeholder="Search here"
+          //backend onChange={handleChange}
+          //backend value={searchInput} 
+          />
+      </div>
 
-        <div className='app__borrow-item-display'>
-          <div className='app__borrow-item'>
+      <div className='app__borrow-item-display'>
+        {borrowList.map((item,index) => (
+            <div className='app__borrow-item'>
             <img src={images.iclicker} alt='image' />
-            <h6 className='head-text'>iClicker</h6>
-            <p> Get now. Return by 11/15/23</p>
-            <button type='button'>Borrow Now</button>
+            <h6 className='head-text'>{item.borrowerName}</h6>
+            <p> Get now. Return by {item.timeEnd}</p>
+            <button type='button' onClick={handleButtonClick}>Borrow Now</button>
           </div>
-          <div className='app__borrow-item'>
-            <img src={images.ipad} alt='image' />
-            <h6 className='head-text'>iPad</h6>
-            <p> Get now. Return by 11/19/23</p>
-            <button type='button'>Borrow Now</button>
-
-          </div>
-          <div className='app__borrow-item'>
-            <img src={images.charger} alt='image' />
-            <h6 className='head-text'>iPhone Charger</h6>
-            <p> Get now. Return by 11/25/23</p>
-            <button type='button'>Borrow Now</button>
-
-          </div>
-          <div className='app__borrow-item'>
-            <img src={images.charger} alt='image' />
-            <h6 className='head-text'>iPhone Charger</h6>
-            <p> Get now. Return by 11/25/23</p>
-            <button type='button'>Borrow Now</button>
-
-          </div>
-         
-          
-          
-        </div>
+        ))}
+        
+        
+       
+        
+        
+      </div>
+  </div>
+    
     </div>
   )
 }
